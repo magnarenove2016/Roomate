38c13b3a7150e6163d3573d4356352ac4eed5297
Asier_Santos_Valcarcel <asiersantosval@gmail.com>
2016-02-01 20:16:46 +0100

    Primer commit del Proyecto

--- web/admin.py
+++ web/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+
+admin.site.register(Usuario)
+admin.site.register(Persona)
+admin.site.register(Perfil)
+admin.site.register(TagValue)
+admin.site.register(FotoPerfil)
+admin.site.register(Tag)
+admin.site.register(Conversacion)
+admin.site.register(Mensaje)
+admin.site.register(Log)
+admin.site.register(Casa)
+admin.site.register(FotoCasa)
+admin.site.register(Habitacion)
+admin.site.register(FotoHabitacion)
--- web/models.py
+++ web/models.py
@@ -1,5 +1,73 @@
-from __future__ import unicode_literals
-
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+class Usuario(models.Model):
+    correo = models.CharField(max_length=200)
+    contrasena = models.CharField(max_length=200)
+    alias = models.CharField(max_length=200)
+    conversaciones = models.ForeignKey("Conversacion")
+
+class Persona(models.Model):
+    perfil = models.ForeignKey("Perfil")
+    identificador = models.CharField(max_length=200)
+    usuariosSimilares = models.ManyToManyField("Persona")
+
+class Perfil(models.Model):
+    fechaNacimiento =models.DateTimeField(default=timezone.now)
+    sexo = models.CharField(max_length=200)
+    trabajadorEstudiante = models.BooleanField()
+    campo = models.CharField(max_length=200)
+    fumador = models.BooleanField()
+    animalCompania = models.CharField(max_length=200)
+    descripcion = models.TextField()
+    zonaBuscada = models.CharField(max_length=200)
+    inicioEstancia = models.DateTimeField(default=timezone.now)
+    finEstancia = models.DateTimeField(default=timezone.now)
+    instrumento = models.CharField(max_length=200)
+
+class TagValue(models.Model):
+    perfil = models.ForeignKey("Perfil")
+    tagName = models.ForeignKey("Tag")
+    value = models.TextField()
+
+class FotoPerfil(models.Model):
+    foto = models.CharField(max_length=200) #path a las fotos
+    perfil = models.ForeignKey("Perfil")
+
+class Tag(models.Model):
+    name = models.CharField(max_length=200)
+
+class Conversacion(models.Model):
+    emisor = models.ForeignKey("Usuario")
+
+class Mensaje(models.Model):
+    conversacion = models.ForeignKey("Conversacion")
+    emisor = models.ForeignKey("Usuario")
+    mensaje = models.TextField()
+
+class Log(models.Model):
+    fecha = models.DateTimeField(default=timezone.now)
+    evento = models.TextField()
+
+class Casa(models.Model):
+    dueno = models.ForeignKey("Persona")
+    ciudad = models.CharField(max_length=200)
+    numHabitaciones = models.IntegerField()
+    numHabitacionesDisponibles = models.IntegerField()
+    descripcion = models.TextField()
+    alquilerPorHabitaciones = models.BooleanField()
+    precioAlquiler = models.FloatField()
+    gastosComplementarios = models.FloatField()
+
+class FotoCasa(models.Model):
+    foto = models.CharField(max_length=200) #path a las fotos
+    casa = models.ForeignKey("Casa")
+
+class Habitacion(models.Model):
+    descripcion = models.TextField()
+    casa = models.ForeignKey("Casa")
+
+class FotoHabitacion(models.Model):
+    foto = models.CharField(max_length=200) #path a las fotos
+    habitacion = models.ForeignKey("Habitacion")
