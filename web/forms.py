c6615fb3f70e01a5330ff9002dfa06e0cf9db733
gbravo002 <gbravo002@ikasle.ehu.es>
2016-02-17 22:06:03 +0100

    Update forms.py
    cambios arrendataroi

--- web/forms.py
+++ web/forms.py
@@ -1,8 +1,15 @@
 from django import forms
 from .models import Usuario
+from web.models import Persona
 
 class UsuarioForm(forms.ModelForm):
 
     class Meta:
         model = Usuario
         fields = ('correo', 'contrasena','alias',)
+        
+class ArrendatarioForm(forms.ModelForm):
+    
+    class Meta:
+        model = Usuario
+        fields = ('arrendatario')
