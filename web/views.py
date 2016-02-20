412d151a33855d5c49d1c08a2c995656e378bdf9
Jon Arzelus <kontrabass32@hotmail.com>
2016-02-20 22:26:43 +0100

    actualizacion

--- web/views.py
+++ web/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render_to_response, render, redirect, get_object_or_404
+from django.template.context_processors import csrf #para verificar la integridad de render_to_response https://docs.djangoproject.com/en/1.8/ref/csrf/
 from django.utils import timezone
 from django.contrib.auth.decorators import login_required
 from django.core.mail import send_mail #para el prototipo de enviar mail
@@ -45,17 +46,21 @@ def register_new_user(request):
         if form.is_valid():
             b = Usuario.objects.filter(correo=request.POST.get('correo'))#si existe un usuario con el mismo correo se guarda en b
             if b.count() == 0: #guarda el usuario sii no existe un usuario con el mismo correo
-            	usuario = form.save(commit=False)
-            	usuario.save()
-            	context = {
-            		'created':request.POST.get('correo')
-            	}
-            	return render_to_response('web/register_new_user.html', context, context_instance=RequestContext(request))
+                usuario = form.save(commit=False)
+                usuario.save()
+                context = {
+                    'created':request.POST.get('correo')
+                }
+                context.update(csrf(request))
+                #return render_to_response('web/register_new_user.html', context, context_instance=RequestContext(request))
+                return render_to_response('web/register_new_user.html', context)
             else:
-            	context = {
-            		'exist':request.POST.get('correo')
-            	}
-            	return render_to_response('web/register_new_user.html', context, context_instance=RequestContext(request))
+                context = {
+                    'exist':request.POST.get('correo')
+                }
+                context.update(csrf(request))
+                #return render_to_response('web/register_new_user.html', context, context_instance=RequestContext(request))
+                return render_to_response('web/register_new_user.html', context)
             return redirect('/',)
     else:
         form = UsuarioForm()
@@ -75,39 +80,39 @@ def add_house(request):
     return render(request, 'web/add_house.html', {'form':form})
 
 '''def recover_password(request):
-	if request.method == "POST":
-		form = RecoverPasswordForm(request.POST)
-		if form.is_valid():
-			Usuario = form.save(commit=False)
-			Usuario.save()
-			user_mail = request.POST.get('correo', '')
-			#send_mail('Password reset', 'Hello: please click the link below to reset your password.', 'admin@roomate.com', [user_mail], fail_silently=False)
-			return redirect('recover_password_done',mail=user_mail)
-	else:
-		form = RecoverPasswordForm()
-	return render(request, 'web/recover_password.html', {'form':form})'''
+    if request.method == "POST":
+        form = RecoverPasswordForm(request.POST)
+        if form.is_valid():
+            Usuario = form.save(commit=False)
+            Usuario.save()
+            user_mail = request.POST.get('correo', '')
+            #send_mail('Password reset', 'Hello: please click the link below to reset your password.', 'admin@roomate.com', [user_mail], fail_silently=False)
+            return redirect('recover_password_done',mail=user_mail)
+    else:
+        form = RecoverPasswordForm()
+    return render(request, 'web/recover_password.html', {'form':form})'''
 
 def recover_password(request):
-	if request.method == "POST":
-		user_mail = request.POST.get('correo')
-		#send_mail('Password reset', 'Hello: please click the link below to reset your password.', 'admin@roomate.com', [user_mail], fail_silently=False)
-		return redirect('recover_password_done',mail=user_mail)
-	else:
-		form = RecoverPasswordForm()
-	return render(request, 'web/recover_password.html', {'form':form})
+    if request.method == "POST":
+        user_mail = request.POST.get('correo')
+        #send_mail('Password reset', 'Hello: please click the link below to reset your password.', 'admin@roomate.com', [user_mail], fail_silently=False)
+        return redirect('recover_password_done',mail=user_mail)
+    else:
+        form = RecoverPasswordForm()
+    return render(request, 'web/recover_password.html', {'form':form})
 
 def recover_password_done(request, mail):
-	context = {
-		'mail': mail
-	}
-	#b = Usuario.objects.get(correo=mail)
-	b = Usuario.objects.filter(correo=mail)
-	if b.count() > 0:
-		#send_mail('Password reset', 'Hello: please click the link below to reset your password.', 'admin@roomate.com', [mail], fail_silently=False)
-		none
-	return render_to_response('web/recover_password_done.html', context, context_instance=RequestContext(request))
+    context = {
+        'mail': mail
+    }
+    #b = Usuario.objects.get(correo=mail)
+    b = Usuario.objects.filter(correo=mail)
+    if b.count() > 0:
+        #send_mail('Password reset', 'Hello: please click the link below to reset your password.', 'admin@roomate.com', [mail], fail_silently=False)
+        none
+    #return render_to_response('web/recover_password_done.html', context, context_instance=RequestContext(request))
+    return render_to_response('web/recover_password_done.html', context)
 
-'''
 def web_prueba(request):
     return render(request, 'index.html', {})
-'''
+
