8a865d3f367ceba050841d71c49fcfeaf0e1b808
isantesteban <igor.santeseban@gmail.com>
2016-03-10 16:59:30 +0100

    Implementado el caso de uso cambiar contraseña

--- Roomate/settings.py
+++ Roomate/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'captcha',
-    'web',    
+    'web',
 	'bootstrap3',
 ]
 
@@ -105,7 +105,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/1.9/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'es'
 
 TIME_ZONE = 'Europe/Madrid'
 
