15a9ca2b60212559730999ea294dd88b299c5ed3
Asier <asiersantosval@gmail.com>
2016-03-15 12:28:39 +0100

    Subida de imagenes funcional, falta mostrarlas/borrarlas

--- web/admin.py
+++ web/admin.py
@@ -4,9 +4,6 @@ from .models import *
 # Mapeamos todos nuestros objetos para poder administrarlos desde /admin
 admin.site.register(FotoPerfil)
 admin.site.register(Tag)
-admin.site.register(Conversacion)
-admin.site.register(Mensaje)
-admin.site.register(Log)
 admin.site.register(Casa)
 admin.site.register(FotoCasa)
 admin.site.register(Habitacion)
