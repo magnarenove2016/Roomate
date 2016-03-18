798f74fafb3b75762d4d2cde1482893797ca5166
Asier Santos Valcárcel <ASantosVal@users.noreply.github.com>
2016-03-18 16:22:44 +0100

    Update settings.py

--- Roomate/settings.py
+++ Roomate/settings.py
@@ -74,16 +74,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'Roomate.wsgi.application'
 
-# Database
-# https://docs.djangoproject.com/en/1.9/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
-
 # Simplified static file serving.
 # https://warehouse.python.org/project/whitenoise/
 
