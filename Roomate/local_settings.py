546d3122ad9bbb3fe051c516715638658e8e5ff1
asier <asiersantosval@gmail.com>
2016-03-18 13:30:40 +0100

    version modificada para funcionar en heroku (pero no en local, cuidado)

--- Roomate/local_settings.py
+++ Roomate/local_settings.py
@@ -3,8 +3,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'roomate',
+        'USER': 'postgremanager',
+        'PASSWORD': 'A71jpC1k',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
